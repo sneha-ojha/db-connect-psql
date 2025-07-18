b470c1bf4affd51655595c84dd3bc1dea676add9
sneha <ojha.sneha021@outlook.com>
2025-07-18 12:25:07 +0530

    Fix: added SSL config for PostgreSQL on Render

--- db/populatedb.js
+++ db/populatedb.js
@@ -16,6 +16,9 @@ async function main() {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
+        ssl: {
+      rejectUnauthorized: false,
+    },
       });
     await client.connect();
     await client.query(SQL);
